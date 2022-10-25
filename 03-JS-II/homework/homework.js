6244bc95430ac318f77c7702843849ae65c2ba82
DISAN - HENRY SEBASTIAN BELALCAZAR MURCIA <HS.BELALCAZAR@policia.gov.co>
2022-10-24 20:00:00 -0500

    seguimos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -11,6 +11,12 @@ function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad >=18){
+    return "Allowed";
+  }
+  if (edad < 18){
+    return  "Not allowed";
+  }
 }
   
 function conection(status) {
@@ -19,15 +25,34 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status === 1){
+    return "Online";
+  }
+  else if (status === 2){
+    return "Away";
+    }
+  else {return "Offline";}
 }
 
 function saludo(idioma) {
-  // Devuelve un saludo en tres diferentes lenguajes:
+  // Devuelve un saludo en tres diferentes lenguajes:np
   // Si "idioma" es "aleman", devuelve "Guten Tag!"
   // Si "idioma" es "mandarin", devuelve "Ni Hao!"
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma ==="aleman"){
+    return "Guten Tag!";
+  }
+  if (idioma === "mandarin"){
+    return "Ni Hao!";
+  }
+  if (idioma==="ingles"){
+    return "Hello!";
+  }
+  else {
+    return "Hola!";
+  }
 }
 
 function colors(color) {
@@ -132,4 +157,4 @@ module.exports = {
   tablaDelSeis,
   tieneTresDigitos,
   doWhile
-};
+}
